f961b2b6fbe126f1747475b202a0da12b7615d41
David <david.binder1993@gmail.com>
2016-06-08 17:33:33 +0200

    1. commit

--- App/app/src/main/java/at/fhooe/mc/android/MainActivity.java
+++ App/app/src/main/java/at/fhooe/mc/android/MainActivity.java
@@ -2,6 +2,7 @@ package at.fhooe.mc.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle _savedInstanceState) {
         super.onCreate(_savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity","Test");
     }
 }
