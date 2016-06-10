eb4f7df6d97cbf17fb689a7e2868e51047a4606f
David <david.binder1993@gmail.com>
2016-06-10 16:07:38 +0200

    Nav Drawer icons erstellt, erstes Demo fragment hinzugefügt

---
+++ LWNReader/app/src/main/java/klinar/kronlachner/binder/app/ListFragment.java
@@ -0,0 +1,32 @@
+package klinar.kronlachner.binder.app;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import klinar.kronlachner.binder.myapplication.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListFragment extends Fragment {
+
+
+    public ListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+}
--- LWNReader/app/src/main/java/klinar/kronlachner/binder/app/MainActivity.java
+++ LWNReader/app/src/main/java/klinar/kronlachner/binder/app/MainActivity.java
@@ -1,8 +1,13 @@
 package klinar.kronlachner.binder.app;
 
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,27 +17,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.List;
 
 import klinar.kronlachner.binder.myapplication.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    public static final String TAG = "Nav Drawer Test";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -42,6 +44,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
     @Override
@@ -80,22 +84,24 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        switch(item.getItemId()){
+            case R.id.nav_search:{
+                Toast.makeText(this, "Search selected",Toast.LENGTH_SHORT).show();
+                ListFragment artice_list_fragment = new ListFragment();
+                FragmentManager manager = getSupportFragmentManager();
+                manager.beginTransaction().replace(R.id.layout_for_fragment, artice_list_fragment).commit();
+            }break;
+            case R.id.nav_favorites:{
+                Toast.makeText(this, "Favorites selected",Toast.LENGTH_SHORT).show();
+            }break;
+            case R.id.nav_kernel:{
+                Toast.makeText(this, "Kernel selected",Toast.LENGTH_SHORT).show();
+            }break;
+            case R.id.nav_security:{
+                Toast.makeText(this, "Security selected",Toast.LENGTH_SHORT).show();
+            }break;
+            default: Log.i(TAG, "unexpected nav_id click encountered in Navigation Drawer");
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
