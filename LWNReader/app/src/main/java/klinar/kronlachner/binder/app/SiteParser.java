f7d667124ad4389cf5b75208986ad53f9332c064
Uwe K <s1510237041@students.fh-hagenberg.at>
2016-06-14 13:33:42 +0200

    updated site parser

--- LWNReader/app/src/main/java/klinar/kronlachner/binder/app/SiteParser.java
+++ LWNReader/app/src/main/java/klinar/kronlachner/binder/app/SiteParser.java
@@ -4,15 +4,6 @@ import java.io.*;
 public class SiteParser{
 	private static final String LWN_URL = "http://lwn.net/";
 	
-	public static void main(String[] args){
-		try{
-			System.out.println(parseFullPage(LWN_URL));
-		}catch(IOException e){
-			e.printStackTrace();
-		}
-
-	}
-	
 	
 	private static String parseFullPage(String url) throws IOException{
 		String currentLine = "";
